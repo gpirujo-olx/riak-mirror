80b797872ad2f8362e595b73b1e69bfbb9a6781c
Guillermo Pereyra Irujo <guillermo@olx.com>
2015-04-21 17:46:17 -0300

    Added S3 side and config file

--- node-uploader/uploader.js
+++ node-uploader/uploader.js
@@ -1,45 +1,93 @@
-var amqp = require('amqp');
-var util = require('util');
+//
+// RABBIT SIDE
+// 
 
-var connection = amqp.createConnection({
-    host: 'localhost',
-    port: 5672,
-    login: 'guest',
-    password: 'guest',
-    vhost: '/'
-});
+function listenToRabbit(connection, queue) {
+    return function(callback) {
+        var amqp = require('amqp');
+        var connection = amqp.createConnection(connection);
 
-// Wait for connection to become established.
-connection.on('ready', function () {
+        // Disconnect cleanly upon Ctrl-C or SIGTERM
+        function closeConnection() {
+            console.log('Disconnecting...');
+            connection.disconnect();
+        }
+        process.on('SIGINT', closeConnection);
+        process.on('SIGTERM', closeConnection);
 
-    connection.exchange('', {
-        type: 'fanout'
-    }, function(exchange) {
-
-        // Use the default 'amq.topic' exchange
-        connection.queue('Atlas', {
-            durable: true,
-            autoDelete: false
-        }, function (q) {
-
-            // Catch all messages
-            q.bind('#');
-
-            console.log('Waiting for messages...');
-
-            // Receive messages
-            q.subscribe(function (message, headers) {
-
-                // Print messages to stdout
-                console.log(util.inspect(message));
-                console.log(util.inspect(headers));
-
+        connection.on('ready', function() {
+            connection.queue(queue, {
+                durable: true,
+                autoDelete: false
+            }, function (queue) {
+                queue.bind('#');
+                queue.subscribe(callback);
+                console.log('Waiting for messages...');
             });
-
         });
 
+        console.log('Connecting...');
+        connection.connect();
+    };
+}
+
+//
+// S3 SIDE
+//
+
+function connectToAmazon(bucket, listenToQueue, s3Key) {
+    var AWS = require('aws-sdk');
+    var s3 = new AWS.S3();
+    listenToQueue(function(message, headers) {
+        var key = s3Key(headers['X-Bucket'], headers['X-Key']);
+        if (headers['X-Deleted'] === 'true') {
+
+            s3.deleteObject({
+                Bucket: s.bucket,
+                Key: key,
+            }, function(err, data) {
+                if (err) console.log(err);
+                else console.log("Successfully deleted " + key);
+            });
+
+        } else {
+
+            s3.putObject({
+                Bucket: s.bucket,
+                Key: key,
+                ContentType: message.contentType,
+                Body: message.data,
+            }, function(err, data) {
+                if (err) console.log(err);
+                else console.log("Successfully uploaded " + key);
+            });
+
+        }
     });
+}
 
-});
+//
+// GO!
+//
 
-connection.connect();
+function s3Key(riakBucket, riakKey) {
+    return riakBucket + '_' + riakKey;
+}
+
+var config = require('config');
+var q = config.get('queue');
+var s = config.get('s3');
+connectToAmazon(
+    s.bucket,
+    listenToRabbit(
+        {
+            host: q.host,
+            port: q.port,
+            login: q.login,
+            password: q.password,
+            vhost: q.vhost,
+        },
+        q.queue
+    ),
+    s3Key
+);
